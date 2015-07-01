63376c1f90c53bc79b4d1b575b3884ca1407d54d
Michael Drummond <michael.drummond@my.liu.edu>
2015-07-01 13:43:07 -0400

    Update Guess.cpp

--- Guess.cpp
+++ Guess.cpp
@@ -13,18 +13,18 @@ int main()
 
 	cout << "Do not input any values (press enter to continue)";
 	cin.get();
-	cout << "Think of a number (press enter)";
+	cout << "Think of a number";
 	cin.get();
-	cout << "Now multiple it by 2 (press enter)";
+	cout << "X 2";
 	cin.get();
 	//Multiple computer guess by two to avoid addition of a odd number
-	cout << "Now Add " << compGuess *2 << " (press enter)";
+	cout << "+ " << compGuess *2;
 	cin.get();
-	cout << "Now Divide it by 2 (press enter)";
+	cout << "Now Divide it by 2";
 	cin.get();
-	cout << "Now subtract the first number you thought of (press enter)";
+	cout << "Now subtract the first number you thought of";
 	cin.get();
-	cout << "Your answer should be.......drum roll please....(press enter)";
+	cout << "Your answer should be.......drum roll please....";
 	cin.get();
 	cout << compGuess;
 	cin.get();
